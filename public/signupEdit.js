85f75d654fc13fd4dbcb0eec653ff586799faff2
Christopher Robinson <CRobinson@student.neumont.edu>
2019-03-10 22:27:22 -0600

    fixed signup page and added authorization

--- public/signupEdit.js
+++ public/signupEdit.js
@@ -1,14 +1,13 @@
-
 var eyes = 'eyes1';
-var eyesCount = 0;
+var eyesIndex = 0;
 var eyesList = ['eyes1','eyes2','eyes3','eyes4','eyes5','eyes6','eyes7','eyes9','eyes10'];
 
 var nose = 'nose2';
-var noseCount = 0;
+var noseIndex = 0;
 var noseList = ["nose2","nose3","nose4","nose5","nose6","nose7","nose8","nose9"];
 
 var mouth = 'mouth4';
-var mouthCount = 0;
+var mouthIndex = 0;
 var mouthList = ["mouth1","mouth10","mouth11","mouth3","mouth5","mouth6","mouth7","mouth9"];
 
 var color = 'DEADBF';
@@ -18,81 +17,88 @@ var hidsrc = document.getElementById('hiddenSrc');
 var colorPicker = document.getElementById('colorPicker');
 
 var Avatar = `https://api.adorable.io/avatars/face/${eyes}/${nose}/${mouth}/${color}/300`;
-img.setAttribute("src", Avatar);
-hidsrc.value = Avatar;
+if (!img.src) {
+    img.src = Avatar;
+    hidsrc.value = Avatar;
+} else {
+    src = img.src;
+    hexRegex = /[abcdef\d]{6}/;
+    colorHex = src.match(hexRegex);
+    colorPicker.value = `#${colorHex[0]}`;
+}
 
 var avatarUpdateColor = () =>{
     color = colorPicker.value;
     color = color.replace('#',"");
     Avatar = `https://api.adorable.io/avatars/face/${eyes}/${nose}/${mouth}/${color}/300`;
-    img.setAttribute("src", Avatar);
+    img.src = Avatar;
     hidsrc.value = Avatar;
 }
 
 var avatarEyesUp = () => {
-    eyesCount++;
-    if(eyesCount > 8){
-        eyesCount = 0;
+    eyesIndex++;
+    if(eyesIndex > 8){
+        eyesIndex = 0;
     }
-    eyes = eyesList[eyesCount];
+    eyes = eyesList[eyesIndex];
     console.log(eyes);
     Avatar = `https://api.adorable.io/avatars/face/${eyes}/${nose}/${mouth}/${color}/300`;
-    img.setAttribute("src", Avatar);
+    img.src = Avatar;
     hidsrc.value = Avatar;
 }
 
 var avatarEyesDown = () => {
-    eyesCount--;
-    if(eyesCount < 0){
-        eyesCount = 8;
+    eyesIndex--;
+    if(eyesIndex < 0){
+        eyesIndex = 8;
     }
-    eyes = eyesList[eyesCount];
+    eyes = eyesList[eyesIndex];
     Avatar = `https://api.adorable.io/avatars/face/${eyes}/${nose}/${mouth}/${color}/300`;
-    img.setAttribute("src", Avatar);
+    img.src = Avatar;
     hidsrc.value = Avatar;
 }
 
 var avatarNoseUp = () => {
-    noseCount++;
-    if(noseCount > noseList.length){
-        noseCount = 0;
+    noseIndex++;
+    if(noseIndex > noseList.length){
+        noseIndex = 0;
     }
-    nose = noseList[noseCount];
+    nose = noseList[noseIndex];
     Avatar = `https://api.adorable.io/avatars/face/${eyes}/${nose}/${mouth}/${color}/300`;
-    img.setAttribute("src", Avatar);
+    img.src = Avatar;
     hidsrc.value = Avatar;
 }
 
 var avatarNoseDown = () => {
-    noseCount--;
-    if(noseCount < 0){
-        noseCount = noseList.length;
+    noseIndex--;
+    if(noseIndex < 0){
+        noseIndex = noseList.length;
     }
-    nose = noseList[noseCount];
+    nose = noseList[noseIndex];
     Avatar = `https://api.adorable.io/avatars/face/${eyes}/${nose}/${mouth}/${color}/300`;
-    img.setAttribute("src", Avatar);
+    img.src = Avatar;
     hidsrc.value = Avatar;
 }
 
 var avatarMouthUp = () => {
-    mouthCount++;
-    if(mouthCount > mouthList.length - 1){
-        mouthCount = 0;
+    mouthIndex++;
+    if(mouthIndex > mouthList.length - 1){
+        mouthIndex = 0;
     }
-    mouth = mouthList[mouthCount];
+    mouth = mouthList[mouthIndex];
     Avatar = `https://api.adorable.io/avatars/face/${eyes}/${nose}/${mouth}/${color}/300`;
-    img.setAttribute("src", Avatar);
+    img.src = Avatar;
     hidsrc.value = Avatar;
 }
 
 var avatarMouthDown = () => {
-    mouthCount--;
-    if(mouthCount < 0){
-        mouthCount = mouthList.length - 1;
+    mouthIndex--;
+    if(mouthIndex < 0){
+        mouthIndex = mouthList.length - 1;
     }
-    mouth = mouthList[mouthCount];
+    mouth = mouthList[mouthIndex];
     Avatar = `https://api.adorable.io/avatars/face/${eyes}/${nose}/${mouth}/${color}/300`;
-    img.setAttribute("src", Avatar);
+    img.src = Avatar;
     hidsrc.value = Avatar;
 }
 
