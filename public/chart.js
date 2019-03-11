d92d14d091ada52252d5d261f11560ca226d6287
Scott Phillips <sphillips@student.neumont.edu>
2019-03-10 20:22:56 -0600

    Bar Graph Works
    - Need to alter color based on img
    - Need to hook up properly to JSON object from db

--- public/chart.js
+++ public/chart.js
@@ -1,4 +1,5 @@
-let ctx = document.getElementById('graphCanvas').getContext('2d');
+let graphCanvas = document.getElementById('graphCanvas')
+let ctx = graphCanvas.getContext('2d');
 
 const clamp = (value, max, min) => {
     return (value > max) ? max : (value < min) ? min : value;
@@ -61,18 +62,134 @@ class GraphBar {
 
     //Scale Bar Relative to Canvas Size - Scalars are Relative Graph Units
     scaleBar(xScalar, yScalar) {
-        return new Vector2(xScalar, yScalar * value);
+        return new Vector2(xScalar, yScalar * this.value);
     }
 
     draw(ctx, position, graphScale) {
-        ctx.fillStyle = color.getFillColor();
-        let scaledBar = scaleBar(graphScale.x, graphScale.y);
+        ctx.fillStyle = this.color.getFillColor();
+        let scaledBar = this.scaleBar(graphScale.x, graphScale.y);
         ctx.fillRect(position.x - (scaledBar.x / 2), position.y - scaledBar.y, scaledBar.x, scaledBar.y);
     }
 }
 
-class BarGraph {
-    constructor() {
-        
+class UserInfo {
+    constructor(imgUrl, messageCount) {
+        this.imgUrl = imgUrl;
+        this.messageCount = messageCount;
+    }
+
+    draw(ctx, position, size) {
+        var image = new Image();
+        image.onload = () => {
+            ctx.drawImage(image, position.x - (size / 2), position.y - (size / 2), size, size);
+        };
+        //image.src = 'https://www.bing.com/th?id=OIP.eOa7NeLUCu7SuY19m1oA1QHaFj&w=263&h=197&c=7&o=5&pid=1.7';
+        image.src = this.imgUrl;
+        console.log('draw');
     }
 }
+
+class BarGraph {
+    constructor(ctx, graphCanvas, barWidth, barMargin, barMaxHeight, barXoffset) {
+        this.ctx = ctx;
+        this.graphCanvas = graphCanvas;
+        this.barWidth = barWidth;
+        this.barMargin = barMargin;
+        this.barMaxHeight = barMaxHeight;
+        this.barXoffset = barXoffset;
+        this.users = [];
+        this.graphBars = [];
+    }
+
+    getUserWithMostPosts() {
+        let i = 0;
+        this.users.forEach(user => {
+            i = user.messageCount > i ? user.messageCount : i;
+        });
+        return i;
+    }
+
+    addUser(user) {
+        this.users.push(user);
+        this.graphBars.push(new GraphBar(user.messageCount, new Color(0, 255, 0)));
+    }
+
+    drawGraph() {
+        let maxMessages = this.getUserWithMostPosts();
+        let barYScale = this.barMaxHeight / maxMessages;
+        let canvasWidth = this.barXoffset + (this.barWidth * this.users.length) + (this.barMargin * this.users.length) + this.barMargin;
+        this.graphCanvas.width = canvasWidth > 500 ? canvasWidth : 500;
+        this.graphCanvas.width = canvasWidth > 500 ? canvasWidth : 500;
+        ctx.clearRect(0, 0, this.graphCanvas.width, this.graphCanvas.height);
+        let barPosition = new Vector2(this.barMargin + this.barWidth / 2 + this.barXoffset, this.barMaxHeight + this.barMargin);
+        let barScale = new Vector2(this.barWidth, barYScale);
+        this.graphBars.forEach(bar => {
+            bar.draw(this.ctx, barPosition, barScale);
+            barPosition.x += (this.barWidth + this.barMargin);
+        });
+        barPosition.x = this.barMargin + this.barWidth / 2 + this.barXoffset;
+        barPosition.y += ((graphCanvas.height - this.barMaxHeight) / 2);
+        this.users.forEach(user => {
+            let imgPosition = new Vector2(barPosition.x, barPosition.y);
+            user.draw(this.ctx, imgPosition, this.barWidth);
+            barPosition.x += (this.barWidth + this.barMargin);
+        });
+        this.drawGraphLines();
+        this.drawGraphText(maxMessages);
+    }
+
+    drawGraphLines() {
+        ctx.beginPath();
+        ctx.lineWidth = this.barMargin * 1.6;
+        let lineStartX = this.barXoffset - ctx.lineWidth;
+        let lineStartY = this.barMargin + (ctx.lineWidth / 2);
+        ctx.moveTo(lineStartX - this.barXoffset * .2, lineStartY);
+        ctx.lineTo(lineStartX, lineStartY);
+        ctx.stroke();
+        ctx.lineTo(lineStartX, this.barMaxHeight + ctx.lineWidth);
+        ctx.stroke();
+        ctx.moveTo(lineStartX - this.barXoffset * .2, this.barMaxHeight + ctx.lineWidth);
+        ctx.lineTo(this.graphCanvas.width - this.barMargin, this.barMaxHeight + ctx.lineWidth);
+        ctx.stroke();
+        ctx.moveTo(lineStartX - this.barXoffset * .2, (this.barMaxHeight + ctx.lineWidth) / 2);
+        ctx.lineTo(lineStartX, (this.barMaxHeight + ctx.lineWidth) / 2);
+        ctx.stroke();
+    }
+
+    drawGraphText(maxMessages) {
+        let lineStartX = this.barXoffset - ctx.lineWidth;
+        let lineStartY = this.barMargin + (ctx.lineWidth / 2);
+        ctx.font = "20px Verdana";
+        let c = new Color(0, 0, 0);
+        ctx.fillStyle = c.getFillColor();
+        ctx.textAlign = 'end';
+        ctx.fillText(maxMessages, lineStartX - (this.barXoffset * .2) - this.barMargin, lineStartY + 10, this.barXoffset * .3);
+        ctx.fillText(maxMessages / 2, lineStartX - (this.barXoffset * .2) - this.barMargin, (this.barMaxHeight + ctx.lineWidth + 10) / 2, this.barXoffset * .3);
+        ctx.fillText(0, lineStartX - (this.barXoffset * .2) - this.barMargin, this.barMaxHeight + ctx.lineWidth + 10, this.barXoffset * .3);
+        ctx.save();
+        ctx.translate(0, 500);
+        ctx.rotate(-Math.PI/2);
+        ctx.textAlign = "center";
+        ctx.fillText("Number of Posts Per User", ((2 * this.graphCanvas.height) - this.barMaxHeight) * .5, (this.barXoffset * .25));
+        ctx.restore();
+    }
+
+}
+
+let graph = new BarGraph(ctx, graphCanvas, 40, 5, 420, 100);
+userList.users.forEach(user => {
+    graph.addUser(new UserInfo(user.imgUrl, user.messageCount));
+});
+// graph.addUser(new UserInfo("https://www.bing.com/th?id=OIP.eOa7NeLUCu7SuY19m1oA1QHaFj&w=263&h=197&c=7&o=5&pid=1.7", 3));
+// graph.addUser(new UserInfo("https://www.bing.com/th?id=OIP.eOa7NeLUCu7SuY19m1oA1QHaFj&w=263&h=197&c=7&o=5&pid=1.7", 3));
+// graph.addUser(new UserInfo("https://www.bing.com/th?id=OIP.eOa7NeLUCu7SuY19m1oA1QHaFj&w=263&h=197&c=7&o=5&pid=1.7", 3));
+// graph.addUser(new UserInfo("https://www.bing.com/th?id=OIP.eOa7NeLUCu7SuY19m1oA1QHaFj&w=263&h=197&c=7&o=5&pid=1.7", 3));
+// graph.addUser(new UserInfo("https://www.bing.com/th?id=OIP.eOa7NeLUCu7SuY19m1oA1QHaFj&w=263&h=197&c=7&o=5&pid=1.7", 3));
+// graph.addUser(new UserInfo("https://www.bing.com/th?id=OIP.eOa7NeLUCu7SuY19m1oA1QHaFj&w=263&h=197&c=7&o=5&pid=1.7", 3));
+// graph.addUser(new UserInfo("https://www.bing.com/th?id=OIP.eOa7NeLUCu7SuY19m1oA1QHaFj&w=263&h=197&c=7&o=5&pid=1.7", 3));
+// graph.addUser(new UserInfo("https://www.bing.com/th?id=OIP.eOa7NeLUCu7SuY19m1oA1QHaFj&w=263&h=197&c=7&o=5&pid=1.7", 3));
+// graph.addUser(new UserInfo("https://www.bing.com/th?id=OIP.eOa7NeLUCu7SuY19m1oA1QHaFj&w=263&h=197&c=7&o=5&pid=1.7", 3));
+// graph.addUser(new UserInfo("https://www.bing.com/th?id=OIP.eOa7NeLUCu7SuY19m1oA1QHaFj&w=263&h=197&c=7&o=5&pid=1.7", 3));
+// graph.addUser(new UserInfo("https://www.bing.com/th?id=OIP.eOa7NeLUCu7SuY19m1oA1QHaFj&w=263&h=197&c=7&o=5&pid=1.7", 3));
+// graph.addUser(new UserInfo("https://www.bing.com/th?id=OIP.eOa7NeLUCu7SuY19m1oA1QHaFj&w=263&h=197&c=7&o=5&pid=1.7", 3));
+graph.drawGraph();
